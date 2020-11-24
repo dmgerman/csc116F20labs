f3e95a63190976812e8708127fba274c9236720d
khaled <khaled.a.helal@ieee.org>
2020-11-24 10:49:13 -0800

    update the Queue class files and the zip file

--- Lab09/code/tester_queue.sh
+++ Lab09/code/tester_queue.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo "Running Test Queue.."
-echo "1" | ../cmake-build-debug/queue_main > tests/queue.out
+echo "Running Queue Test.."
+echo "1" | ./queue > tests/queue.out
 diff tests/expected/queue.expected tests/queue.out
